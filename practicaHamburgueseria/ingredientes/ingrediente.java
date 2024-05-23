6d4173d5d46c070f5ca5ece678f3a931ca0bcdb2
Valentino Cattaneo Luna <131176387+ValentinoCattaneoLuna@users.noreply.github.com>
2024-05-23 00:33:42 -0300

    Delete practicaHamburgueseria/ingredientes/ingrediente.java

--- practicaHamburgueseria/ingredientes/ingrediente.java
+++
@@ -1,7 +0,0 @@
-package practicaHamburgueseria.ingredientes;
-
-public abstract class ingrediente implements agregable {
-    protected int precio;
-
-    public int getPrecio() {return this.precio;}
-}
